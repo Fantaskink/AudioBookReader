74844bc29f533def082474eee8a3a90cce0ff701
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2023-03-26 23:11:11 +0200

    Added support for going to next and previous page

--- my-electron-app/src/preload.js
+++ my-electron-app/src/preload.js
@@ -3,6 +3,8 @@ const pdfjsLib = require('pdfjs-dist');
 pdfjsLib.GlobalWorkerOptions.workerSrc = './public/pdfjs/build/pdf.worker.js';
 
 const fs = require('fs');
+let currentPage = 1;
+let path = "";
 
 // Expose ipcRenderer to the renderer process
 contextBridge.exposeInMainWorld('ipcRenderer', {
@@ -19,28 +21,45 @@ contextBridge.exposeInMainWorld('pdfjsLib', pdfjsLib);
 
 // Expose loadPDF to the renderer process
 contextBridge.exposeInMainWorld('loadPDF', (filePath) => {
-    
+
+    // Check if a canvas with id "pdf-container" exists and delete it
+    var existingCanvas = document.getElementById('pdf-container');
+    if (existingCanvas) {
+        existingCanvas.remove();
+    }
+
+    // Set the canvas dimensions to match the PDF page
+    var canvas = document.createElement('canvas');
+    canvas.id = 'pdf-container';
+    document.body.appendChild(canvas);
+
+    path = filePath;
+    loadPage(path, currentPage);
+});
+
+contextBridge.exposeInMainWorld('loadPage', (increment) => {
+    loadPage(path, currentPage + increment);
+
+    currentPage = currentPage + increment;
+});
+
+
+function loadPage(filePath, pageNum) {
     const pdfData = new Uint8Array(fs.readFileSync(filePath));
 
-    
-    // Set the canvas dimensions to match the PDF page
-    var canvas = document.getElementById('pdf-container');
+    var canvas = document.getElementById("pdf-container");
     var context = canvas.getContext('2d');
-
 
     // Load the PDF file from the typed array
     pdfjsLib.getDocument(pdfData.buffer).promise.then(pdf => {
 
-        pdf.getPage(1).then(function (page) {
+        pdf.getPage(pageNum).then(function (page) {
           var scale = 1.5;
           var viewport = page.getViewport({ scale: scale, });
-
           
           canvas.height = viewport.height;
           canvas.width = viewport.width;
 
-
-      
           // Render the PDF page on the canvas
          let rendertask = page.render({
             canvasContext: context,
@@ -49,4 +68,5 @@ contextBridge.exposeInMainWorld('loadPDF', (filePath) => {
           
         });
     });
-});
+
+}
--- my-electron-app/src/renderer.js
+++ my-electron-app/src/renderer.js
@@ -1,11 +1,21 @@
 // Receive the text content of the PDF file from the main process
 ipcRenderer.on('load-pdf', (filePath) => {
     loadPDF(filePath);
-  
+});
+
+ipcRenderer.on('next-page', (increment) => {
+  loadPage(increment)
+});
+
+ipcRenderer.on('previous-page', (increment) => {
+  loadPage(increment)
 });
 
 const loadBookBtn = document.getElementById('open-book-link');
 const loadAudioBtn = document.getElementById('open-audio-link');
+
+const prevPageBtn = document.getElementById('previous-page');
+const nextPageBtn = document.getElementById('next-page');
 
 loadBookBtn.addEventListener('click', function () {
   ipcRenderer.send('load-book');
@@ -13,4 +23,12 @@ loadBookBtn.addEventListener('click', function () {
 
 loadAudioBtn.addEventListener('click', function () {
   ipcRenderer.send('load-audio');
+});
+
+prevPageBtn.addEventListener('click', function () {
+  loadPage(-1);
+});
+
+nextPageBtn.addEventListener('click', function () {
+  loadPage(1);
 });
